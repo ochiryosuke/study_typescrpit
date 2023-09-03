cfe9ba106e753e2ac9b0ec1fa7c55fa4203ac169
Ochi Ryosuke <ht21a023@oecu.jp>
2023-09-04 01:13:42 +0900

    Fix all tsfile

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import world from './world';
 
 const root: HTMLElement | null = document.getElementById('root');
 const world_istance: world = new world('Hello Typescript');
- world_istance.setMessage(root);
+world_istance.setMessage(root);
